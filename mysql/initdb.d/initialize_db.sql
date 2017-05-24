63e3064324a079ccea30d824d6b2f56dd7c0c783
yatanokarasu <sync.outerphase@gmail.com>
2017-05-25 00:45:17 +0900

    Update EOL style
    Signed-off-by: yatanokarasu <sync.outerphase@gmail.com>

--- mysql/initdb.d/initialize_db.sql
+++ mysql/initdb.d/initialize_db.sql
@@ -4,8 +4,8 @@ SET @@SESSION.SQL_LOG_BIN=0;
 CREATE USER 'sonar'@'%'   IDENTIFIED BY 'sonar';
 CREATE USER 'redmine'@'%' IDENTIFIED BY 'redmine';
 
-CREATE DATABASE `sonar`   DEFAULT CHARACTER SET utf8  COLLATE utf8_generate_ci;
-CREATE DATABASE `redmine` DEFAULT CHARACTER SET utf8  COLLATE utf8_generate_ci;
+CREATE DATABASE `sonar`   DEFAULT CHARACTER SET utf8  COLLATE utf8_general_ci;
+CREATE DATABASE `redmine` DEFAULT CHARACTER SET utf8  COLLATE utf8_general_ci;
 
 GRANT ALL ON `sonar`.*   TO 'sonar'@'%'   WITH GRANT OPTION;
 GRANT ALL ON `redmine`.* TO 'redmine'@'%' WITH GRANT OPTION;
