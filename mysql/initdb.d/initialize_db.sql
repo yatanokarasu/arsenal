46c75f62ce4e01e3d5f5e4bf27da0d980dfc326f
yatanokarasu <sync.outerphase@gmail.com>
2017-06-10 00:04:43 +0900

    Add Rancher container
    Signed-off-by: yatanokarasu <sync.outerphase@gmail.com>

--- mysql/initdb.d/initialize_db.sql
+++ mysql/initdb.d/initialize_db.sql
@@ -2,16 +2,19 @@
 SET @@SESSION.SQL_LOG_BIN=0;
 
 CREATE USER 'sonar'@'%'     IDENTIFIED BY 'sonar';
+CREATE USER 'cattle'@'%'    IDENTIFIED BY 'cattle';
 CREATE USER 'redmine'@'%'   IDENTIFIED BY 'redmine';
 CREATE USER 'rundeck'@'%'   IDENTIFIED BY 'rundeck';
 CREATE USER 'gitbucket'@'%' IDENTIFIEd BY 'gitbucket';
 
 CREATE DATABASE `sonar`     DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `cattle`    DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE `redmine`   DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE `rundeck`   DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE `gitbucket` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 GRANT ALL ON `sonar`.*     TO 'sonar'@'%'     WITH GRANT OPTION;
+GRANT ALL ON `cattle`.*    TO 'cattle'@'%'    WITH GRANT OPTION;
 GRANT ALL ON `redmine`.*   TO 'redmine'@'%'   WITH GRANT OPTION;
 GRANT ALL ON `rundeck`.*   TO 'rundeck'@'%'   WITH GRANT OPTION;
 GRANT ALL ON `gitbucket`.* TO 'gitbucket'@'%' WITH GRANT OPTION;
